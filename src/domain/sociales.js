9f403aa8d85d2cf0c2a068d8f0786c9169a014dd
Pablo Tedesco <tedesco8@hotmail.com>
2021-01-27 17:25:07 -0300

    controlando palabras para responder

--- src/domain/sociales.js
+++ src/domain/sociales.js
@@ -9,47 +9,63 @@ function social() {
 
   bot.on("text", (msg) => {
     let text = msg.text.toLocaleLowerCase();
+
     const options = {
       reply_to_message_id: msg.message_id,
     };
-    switch (text) {
-      case "invitar":
-        bot.sendMessage(
-          msg.from.id,
-          `Hola ${msg.from.first_name}, aquí está el link para que puedas invitar: https://t.me/joinchat/UzonQGUkz56QIkj5`,
-          options
-        );
-        break;
-      case "slack":
-        bot.sendMessage(
-          msg.from.id,
-          `Hola ${msg.from.first_name}, aquí está el link de nuestro Slack: https://latamdev.slack.com/`,
-          options
-        );
-        break;
-      case "linkedin":
-        bot.sendMessage(
-          msg.from.id,
-          `Hola ${msg.from.first_name}, tenemos un grupo en LinkedIn, únete a nosotros en el siguiente enlace: https://www.linkedin.com/groups/12245320/`,
-          options
-        );
-        break;
-      case "discord":
-        bot.sendMessage(
-          msg.from.id,
-          `Hola ${msg.from.first_name}, puedes encontrarnos en Discord aquí: https://www.linkedin.com/groups/12245320/`,
-          options
-        );
-        break;
-      default:
-        bot.sendMessage(
-          msg.from.id,
-          `Hola ${msg.from.first_name}, no tengo registros de esta comunidad, puedes hablar con algún administrador o agregarla a mi código fuente ubicado en https://github.com/tedesco8/bot-telegram-node.git`,
-          options
-        );
-        break;
+
+    function control(text) {
+      let txt = ["invitar", "slack", "linkedin", "discord"];
+      let boolean = false
+      txt.forEach( i => {
+        if(i === text) {
+          boolean = true
+        }
+      })
+      return boolean
+    }
+
+    if (control) {
+      console.log(control)
+      switch (text) {
+        case "invitar":
+          bot.sendMessage(
+            msg.from.id,
+            `Hola ${msg.from.first_name}, aquí está el link para que puedas invitar: https://t.me/joinchat/UzonQGUkz56QIkj5`,
+            options
+          );
+          break;
+        case "slack":
+          bot.sendMessage(
+            msg.from.id,
+            `Hola ${msg.from.first_name}, aquí está el link de nuestro Slack: https://latamdev.slack.com/`,
+            options
+          );
+          break;
+        case "linkedin":
+          bot.sendMessage(
+            msg.from.id,
+            `Hola ${msg.from.first_name}, tenemos un grupo en LinkedIn, únete a nosotros en el siguiente enlace: https://www.linkedin.com/groups/12245320/`,
+            options
+          );
+          break;
+        case "discord":
+          bot.sendMessage(
+            msg.from.id,
+            `Hola ${msg.from.first_name}, puedes encontrarnos en Discord aquí: https://www.linkedin.com/groups/12245320/`,
+            options
+          );
+          break;
+        default:
+          bot.sendMessage(
+            msg.from.id,
+            `Hola ${msg.from.first_name}, no tengo registros de esta comunidad, puedes hablar con algún administrador o agregarla a mi código fuente ubicado en https://github.com/tedesco8/bot-telegram-node.git`,
+            options
+          );
+          break;
+      }
     }
   });
 }
 
-module.exports = {social}
+module.exports = { social };
